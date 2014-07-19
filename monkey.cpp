f67ecaab4d3691ad89736c3d5b01dd8f6bd6830d
Leios <jrs0037@auburn.edu>
2014-07-18 20:14:31 -0500

    removed unnecessary lines.

--- monkey.cpp
+++ monkey.cpp
@@ -20,28 +20,15 @@ void init_rand(){srand((unsigned)time(0));}
 
 void gen_rand(int window, string wword){
     const char alphabet[] =
-//        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "abcdefghijklmnopqrstuvwxyz";
     string check, test;
-/*    
-    for(int i=0; i < window; i++){
-        s[i] =  alphabet[rand() % (sizeof(alphabet) -1)];}
 
-    s[window] = 0;
-    cout << s << endl;
-*/
-//I need to pull s[i's] into strings and chack against the dictionary.
-//and then move the window forward if it exceeds it's limit
     for(int i = 0; i < 10000; i++){
     wword = alphabet[rand() % (sizeof(alphabet) -1)];
     check.append(wword);
     dict_check(check);    
-//    cout << check << endl;
     if(check.size() == window){
         check.erase(0,1);}
     }
-
-        
 }
 
-    
