1eb0f2bf2b55a4d4205daf030302d6ead73482f2
Leios <jrs0037@auburn.edu>
2014-07-19 11:42:15 -0500

    Adding info about the imp filesystem. It doesn't quite work yet, but I think it will be useful. For now, just keep using chimp as you normally would.

---
+++ impff.cpp
@@ -0,0 +1,19 @@
+/*-------------impff.cpp------------------------------------------------------//
+*
+*              Infinite Monkey Poet File Format (impff)
+*
+* This file contains information about reading imp files, which can store data
+* in an n-dimensional way. I do not guarantee the efficiency of this file 
+* format. It will probably be incredibly slow.
+*
+*-----------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void impwrite(
+
+
+void impread(
---
+++ impff.h
@@ -0,0 +1,7 @@
+/*-------------impff.h--------------------------------------------------------//
+*
+*              Infinite Monkey Poet File Format
+*
+* This file is meant to be used generally on any file that wishes to use impff
+*
+*-----------------------------------------------------------------------------*/
